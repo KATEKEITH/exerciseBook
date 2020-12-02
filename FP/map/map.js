d1f4a07c92bfeecb16c69aa18a2650e7771ef50a
sbm3539 <sbm3539@naver.com>
2020-12-02 12:08:27 +0900

    Feat Array.prototype.map 메소드 구현

---
+++ FP/map/map.js
@@ -0,0 +1,11 @@
+function map(arr, func) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(func(arr[i], i));
+  }
+  return result;
+}
+
+const arr = [1, 2, 3, 4, 5];
+console.log(map(arr, (value) => value + 2));
+console.log(arr.map((value) => value + 2));
